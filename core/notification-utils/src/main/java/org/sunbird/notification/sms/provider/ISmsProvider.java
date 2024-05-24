8ede49850551f922827b996589c538e3a8823b09
Devaj Kumar <devaj.kumar@trigyn.com>
2024-05-24 19:26:09 +0530

    157628 logger removed

--- core/notification-utils/src/main/java/org/sunbird/notification/sms/provider/ISmsProvider.java
+++ core/notification-utils/src/main/java/org/sunbird/notification/sms/provider/ISmsProvider.java
@@ -17,9 +17,7 @@ public interface ISmsProvider {
     Map<String, Map<String, String>> smsTemplateConfig = SmsTemplateUtil.getSmsTemplateConfigMap();
     Map<String, String> providerTemplateConfig = smsTemplateConfig.get(provider);
     for (Map.Entry<String, String> entry : providerTemplateConfig.entrySet()) {
-      System.out.println("**************Template id :"+entry.getValue()+"********* Template Key :"+entry.getKey());
       String pattern = entry.getKey().replaceAll("\\$[^ .]+", ".*?");
-      System.out.println("**************Pattern :"+pattern);
       if (sms.matches(pattern)) {
         System.out.println("SMS Pattern matched for :"+entry.getValue());
         return entry.getValue();
