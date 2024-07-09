5281adeb3496d63d4644d5f5679fe5c39deb12fc
Devaj Kumar <devaj.kumar@trigyn.com>
2024-07-09 11:02:07 +0530

    195124 added api in config file

--- service/src/main/java/org/sunbird/actor/chatwithbooks/ChatWithBooksActor.java
+++ service/src/main/java/org/sunbird/actor/chatwithbooks/ChatWithBooksActor.java
@@ -2,9 +2,11 @@ package org.sunbird.actor.chatwithbooks;
 
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.keys.JsonKey;
+import org.sunbird.logging.LoggerUtil;
 import org.sunbird.request.Request;
 import org.sunbird.service.chatwithbooks.ChatWithBooksService;
 import org.sunbird.service.chatwithbooks.impl.ChatWithBooksServiceImpl;
+import org.sunbird.service.feed.impl.FeedServiceImpl;
 import org.sunbird.telemetry.dto.TelemetryEnvKey;
 import org.sunbird.util.Util;
 
@@ -12,11 +14,14 @@ import java.util.Map;
 
 public class ChatWithBooksActor extends BaseActor {
 
+    private final LoggerUtil logger = new LoggerUtil(ChatWithBooksActor.class);
     private final ChatWithBooksService userService = ChatWithBooksServiceImpl.getInstance();
 
     public void onReceive(Request request) throws Throwable {
         Util.initializeContext(request, TelemetryEnvKey.USER);
         String operation = request.getOperation();
+        logger.info("Operation ChatWithBooksActor : "+operation);
+        logger.info("Request ChatWithBooksActor : "+request);
         switch (operation) {
             case "chatWithBooksSave":
                 chatWithBooksSave(request);
@@ -40,6 +45,7 @@ public class ChatWithBooksActor extends BaseActor {
         //Saving the chat query in DB
         logger.info("Insert Query :"+chatMapWithBooksMap.toString());
         userService.chatWithBookSave(chatMapWithBooksMap,actorMessage.getRequestContext());
+        logger.info("Success Data inserted into DB");
     }
 
     private void chatWithBooksRead(Request actorMessage) {
